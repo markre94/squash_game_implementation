15369419ae339bcbe244d622a9e700448b8a1fe1
Marcin <markre94@icloud.com>
2020-08-23 09:32:37 +0200

    Added a functional paddle, fix the bottom border and add data science to play the game

--- sqash.py
+++ sqash.py
@@ -2,22 +2,45 @@
 
 import pygame, sys
 
+# global variables
+
 s_width = 1200
 s_height = 600
 border = 20
 ball_radius = 10
+velo = -10
+framerate = 35
 
 
 class Ball:
-    def __init__(self, x, y):
+    def __init__(self, x, y, vel_x, vel_y):
         self.x = x
         self.y = y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
 
     def show_ball(self, surface, color):
         pygame.draw.circle(surface=surface, color=color, center=(self.x, self.y), radius=ball_radius)
 
     def update(self):
-        pass
+        self.show_ball(screen, pygame.Color("black"))
+        self.x += self.vel_x
+        self.y += self.vel_y
+        # self.show_ball(screen, fg_color)
+
+        if self.y <= (border + ball_radius) \
+                or self.y >= (s_height - border - ball_radius):
+            self.vel_y = self.vel_y * -1
+            # self.x += self.vel_x
+
+        elif self.x == (border + ball_radius):
+            self.vel_x = self.vel_x * - 1
+            # self.y += self.vel_y
+
+        elif self.x  == ball_radius + s_width - Paddle.paddle_width and abs(self.y - pdd.y) <= Paddle.paddle_height//2:
+            self.vel_x = self.vel_x * -1
+
+        self.show_ball(screen, fg_color)
 
 
 class Paddle:
@@ -32,7 +55,14 @@ class Paddle:
         pygame.draw.rect(surface, color, (self.x, self.y, Paddle.paddle_width, Paddle.paddle_height))
 
     def update(self):
-        pass
+        self.show_paddle(screen, pygame.Color("black"))
+
+        if pygame.key.get_pressed() [pygame.K_UP]:
+            self.y -= 10
+        if pygame.key.get_pressed() [pygame.K_DOWN]:
+            self.y += 10
+
+        self.show_paddle(screen, fg_color)
 
 
 pygame.init()
@@ -44,17 +74,24 @@ pygame.draw.rect(screen, fg_color, (0, 0, s_width, border))
 pygame.draw.rect(screen, fg_color, (0, s_height, s_width, -border))
 pygame.draw.rect(screen, fg_color, (0, 0, border, s_height))
 
-ball = Ball(s_width - ball_radius - Paddle.paddle_width, s_height // 2 - ball_radius)
+ball = Ball(s_width - ball_radius - Paddle.paddle_width, s_height // 2, velo, velo)
 ball.show_ball(screen, color=pygame.Color("white"))
 
-pdd = Paddle(s_width - Paddle.paddle_width, s_height // 2 - Paddle.paddle_height)
+pdd = Paddle(s_width - Paddle.paddle_width, s_height // 2 - Paddle.paddle_height // 2)
 pdd.show_paddle(screen, fg_color)
 
-pygame.display.flip()
-
+clock = pygame.time.Clock()
 # game loop
+
 run = True
+
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
+    e = pygame.event.poll()
+
+    if e.type == pygame.QUIT:
+        sys.exit(0)
+
+    clock.tick(framerate)
+    pygame.display.flip()
+    ball.update()
+    pdd.update()
